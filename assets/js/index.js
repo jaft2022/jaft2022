e0d8d00cfda4a3c30d03f7ca28bf579294127614
javier <javier.felix@gcabsa.com.mx>
2022-04-01 11:19:58 -0700

    Se hizo la página web :D

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+(function(){
+    const header = document.getElementById("header");
+    const screenHeight = document.getElementsByTagName("body")[0].clientHeight;
+    let defaultSize = 50; /* Altura inicial del headeer */
+    const interval = setInterval(()=>{
+        /* Aumentamos el tamaño de 10 en 10 cada 10 milisegundos */
+        header.style.height = defaultSize+"px";
+        defaultSize += 10
+        /* Detenemos el interval si este es mayor a la ventana */
+        if(defaultSize > screenHeight){
+            clearInterval(interval)
+        }
+    }, 10)
+})()
+
